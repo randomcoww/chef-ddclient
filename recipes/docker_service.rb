6a6f34bb4b32febd598929d6b95d40674f456573
randomcoww <etherblimp@gmail.com>
2017-04-15 10:58:04 -0700

    remove service recipe

--- recipes/docker_service.rb
+++
@@ -1,7 +0,0 @@
-service "ddclient" do
-  start_command "/etc/init.d/ddclient start"
-  stop_command "/etc/init.d/ddclient stop"
-  restart_command "/etc/init.d/ddclient restart"
-  status_command "/etc/init.d/ddclient status"
-  action [:start]
-end
